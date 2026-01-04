c92c25483fa3116c538717014c83bd3b1c47dccd
Suraj Prajapati <sp0640481@gmail.com>
2026-01-04 10:40:35 +0530

    exit attempt #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-       
-      
+
+        Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.print("$ ");
 
-            Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine();
-            
-            if(command.equals("exit")) return;
+
+            if (command.equals("exit")) {
+                break;
+            }
 
             System.out.println(command + ": command not found");
 
-
-
-            scanner.close();
         }
 
-        
+        scanner.close();
+
     }
 }
