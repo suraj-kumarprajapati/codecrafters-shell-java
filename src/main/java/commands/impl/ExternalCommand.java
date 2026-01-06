d643cc3678fef0556bf1300caec417a5599e0c8f
Suraj Prajapati <sp0640481@gmail.com>
2026-01-06 12:35:02 +0530

    redirection #7

--- src/main/java/commands/impl/ExternalCommand.java
+++ src/main/java/commands/impl/ExternalCommand.java
@@ -1,5 +1,7 @@
 package commands.impl;
 
+import java.io.PrintStream;
+
 import commands.ICommand;
 
 public class ExternalCommand implements ICommand {
@@ -12,21 +14,32 @@ public class ExternalCommand implements ICommand {
 
     @Override
     public void execute(String[] args) {
+
+        PrintStream originalOut = System.out;
+        PrintStream originalErr = System.err;
         
         try {
             // Replace command name with absolute executable path
             args[0] = command; 
             
             ProcessBuilder pb = new ProcessBuilder(args);
-            
+
             // show output to the parent process's stream
             // pb.inheritIO();  
+
+            pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            pb.redirectError(ProcessBuilder.Redirect.INHERIT);
 
             Process process = pb.start();
             process.waitFor();
         }
         catch(Exception e) {
 
+        }
+        finally {
+            // Restore Java streams 
+            System.setOut(originalOut);
+            System.setErr(originalErr);
         }
     }
 
